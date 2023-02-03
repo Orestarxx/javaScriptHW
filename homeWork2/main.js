9b0221fc1c1990bf2895f9751cd9ca88956c6ccb
Orestarxx <oresto322@gmail.com>
2023-02-03 18:11:24 +0200

    commit

--- homeWork2/main.js
+++ homeWork2/main.js
@@ -65,14 +65,89 @@ if(a !== 0){
 }
 // - Дано змінну time яка рівна числу від 0 до 59. Потрібно написати код, який перевірить, до якої четверті години попадає число
 // (в першу, другу, третю или четверту частину години).
+let time = 60;
+if(time => 0 && time <= 15 ){
+    console.log(`first`);
+}else if(time > 15 && time <= 30){
+    console.log(`second`);
+}else if(time > 30 && time <= 45){
+    console.log(`third`);
+}else if(time >45 && time <= 59){
+    console.log(`forth`);
+}else{
+    console.log(`none of them`);
+}
+
 // - У змінній day дано якесь число від 1 до 31. Потрібно визначити, у яку половину(декаду) місяця потрапляє це число (у першу, другу чи третю).
+let day = 1 ;
+if(day => 1 && day <= 10){
+    console.log(`first`);
+}else if(day > 10 && day <= 20){
+    console.log(`second`);
+}else if(day > 20 && day <= 31){
+    console.log(`third`);
+}else{
+    console.log(`none of them`);
+}
 // - Скласти розклад на тиждень за домопоги switch. Користувач вводить порядковий номер дня тижня і на екрані відображається інфа що заплановано на цей день (можна замість плану на день, назву дня англійською).
+let dayOfWeek =`Sunday`;
+switch (dayOfWeek){
+    case 'Monday':
+        console.log(`go to gym`);
+        break;
+    case 'Tuesday':
+        console.log(`go to walk`);
+        break;
+    case 'Wednesday':
+        console.log(`play Xbox`);
+        break;
+    case 'Thursday':
+        console.log(`to code`);
+        break;
+    case 'Friday':
+        console.log(`play football`);
+        break;
+    case 'Saturday':
+        console.log(`learn code`);
+        break;
+    case 'Sunday':
+        console.log(`be ready to next week`);
+        break;
+    default:
+        console.log(`write the current day`);
+}
+
 //     - Користувач вводить або має два числа.
 //         Потрібно знайти та вивести максимальне число з тих двох .
 //         Також потрібно врахувати коли введені рівні числа.
-//
+ let numberOne = 1;
+ let numberTwo = 1;
+ if(numberOne > numberTwo){
+     console.log(numberOne);
+ }else if(numberTwo > numberOne){
+     console.log(numberTwo);
+ }else if (numberTwo === numberOne){
+     console.log(`equal`);
+ }else {
+     console.log(`only numbers`);
+ }
 //     - є змінна х, яка може прийняти будь-яке значення (стрінг, число, undefined, null  і тд включно). Напишіть код який,
 //         за допомоги  оператора || буде присвоювати змінній х значення "default"  якщо значення змінної х являється falsy (хибноподібні, тобто приводиться до false)
-//
-//
+
+
 //     - з файлу arrays.js (лежить в папці 2023 plan ) взяти масив coursesAndDurationArray. За допомоги іф перевірити кожен його елемент на тривалість навчання. У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер".
+let coursesAndDurationArray = [
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+ for (course of coursesAndDurationArray){
+     if(course.monthDuration > 5){
+         console.log(`супер`);
+     }else {
+         console.log(`тоже не погано`);
+     }
+ }
